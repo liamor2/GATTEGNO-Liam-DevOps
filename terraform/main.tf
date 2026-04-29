48c0a9017144cd389f870b9813f219918c8c5129
liamor2 <git.com@liam.gattegno.net>
2026-04-29 16:28:44 +0200

    Refactor terraform to use template files instead.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,21 @@
 terraform {
   required_version = ">= 1.4.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
 }
 
-resource "terraform_data" "proof_of_execution" {
-  input = {
-    executed_at = timestamp()
-  }
+locals {
+  executed_at = timestamp()
+}
 
-  triggers_replace = timestamp()
-
-  provisioner "local-exec" {
-    command = "printf '%s\n' 'Executed at ${self.input.executed_at}. Created with Terraform.' > ../proof_of_execution"
-  }
+resource "local_file" "proof_of_execution" {
+  filename = "${path.module}/../proof_of_execution"
+  content = templatefile("${path.module}/templates/proof_of_execution.tftpl", {
+    executed_at = local.executed_at
+  })
 }
