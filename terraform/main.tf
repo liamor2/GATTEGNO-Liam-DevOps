d3a2b1de31d6bf9a3d4c97972bcea3773418659f
liamor2 <git.com@liam.gattegno.net>
2026-04-29 16:36:32 +0200

    Added more variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,20 @@ terraform {
 
 locals {
   executed_at = timestamp()
+  yippee      = "YIPPEEE"
+}
+
+variable "commit_name" {
+  description = "Name or label for the commit that triggered this workflow."
+  type        = string
+  default     = "unknown commit"
 }
 
 resource "local_file" "proof_of_execution" {
   filename = "${path.module}/../proof_of_execution"
   content = templatefile("${path.module}/templates/proof_of_execution.tftpl", {
     executed_at = local.executed_at
+    commit_name = var.commit_name
+    yippee      = local.yippee
   })
 }
